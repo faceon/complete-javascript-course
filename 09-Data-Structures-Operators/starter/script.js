94dcfbe8dae25e46974196932177268bd1982998
Seongwkim <faceon@gmail.com>
2023-01-15 20:51:22 +0900

    09 challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,13 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex = 0, mainIndex }) {
+    console.log(this.starterMenu[starterIndex], this.mainMenu[mainIndex]);
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +34,112 @@ const restaurant = {
     },
   },
 };
+
+// const res = restaurant;
+// res.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole',
+//   mainIndex: 1,
+//   starterIndex: 2,
+// });
+
+// res.orderDelivery({
+//   mainIndex: 2,
+// });
+
+// const add = function (...nums) {
+//   console.log(nums.reduce((total, num) => total + num, 0));
+// };
+
+// add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 20,
+// };
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni',
+// };
+
+// Challenge #1
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+const [gk, ...fieldPlayers] = players1;
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+const [...allPlayers] = [...players1, ...players2];
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+const { team1, x: draw, team2 } = game.odds;
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+function printGoals(...players) {
+  console.log(...players);
+  console.log(players.length, 'goals');
+}
+printGoals(game.scored);
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+const { team1: team1odds, team2: team2odds } = game.odds;
+team1odds < team2odds && console.log('team1');
+team1odds > team2odds && console.log('team2');

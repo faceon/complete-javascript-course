53c4d5975f2252a58c27bc4b7aba886d81bef648
Seongwkim <faceon@gmail.com>
2023-01-23 21:57:59 +0900

    13 node element append and remove

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +30,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies <button class="btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+header.append(message);
+// header.prepend(message.cloneNode(true));
+// header.after(message);
+
+const btnCloseCookie = document.querySelector('.btn--close-cookie');
+btnCloseCookie.addEventListener('click', () => message.remove());

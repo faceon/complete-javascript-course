aba14fc17ec999bd236183af7d82fb946e8abdae
Seongwkim <faceon@gmail.com>
2023-01-25 18:54:00 +0900

    13 tabContainer

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -80,14 +80,104 @@ btnScrollTo.addEventListener('click', e => {
   // s1.scrollIntoView({ behavior: 'smooth' });
 });
 
-const alertH1 = e => {
-  console.log('You are reading H1');
-};
-const h1 = document.querySelector('h1');
+// const alertH1 = e => {
+//   console.log('You are reading H1');
+// };
+// const h1 = document.querySelector('h1');
 // h1.onmouseenter = () => console.log('Triggered by onmouseneter');
-h1.addEventListener('mouseenter', alertH1);
+// h1.addEventListener('mouseenter', alertH1);
 
 //// Event Bubbling
 const randomInt = ({ min = 0, max = 255 }) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () => `rgb(${Array(3).fill(0).map(randomInt).join(',')})`;
+const changeColor = function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(this.classList[0].padStart(12, ' '), this === e.currentTarget);
+  // e.stopPropagation();
+};
+
+// document.querySelector('.nav__link').addEventListener('click', changeColor);
+// document.querySelector('.nav__links').addEventListener('click', changeColor);
+// document.querySelector('.nav').addEventListener('click', changeColor);
+
+//// Page navigation: old school, low performance
+const scrollToHref = function (e) {
+  e.preventDefault();
+  const href = document.querySelector(this.getAttribute('href'));
+  href?.scrollIntoView({ behavior: 'smooth' });
+};
+
+// document
+//   .querySelectorAll('.nav__link')
+//   .forEach(elem => elem.addEventListener('click', scrollToHref));
+
+// Page navigation: using bubbling
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target.classList);
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    document
+      .querySelector(e.target.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//// DOM traversing
+// const h1 = document.querySelector('h1');
+
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'blue';
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+// console.log(h1.closest('.header'));
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+//// Operations buttons using DOM traversing
+// first code
+// document.querySelector('#section--2').addEventListener('click', function (e) {
+//   if (e.target.classList.contains('operations__tab')) {
+//     // set active this tab while deactivating other siblings
+//     [...e.target.parentElement.children].forEach(elem =>
+//       elem.classList.remove('operations__tab--active')
+//     );
+//     e.target.classList.add('operations__tab--active');
+//     // query content div and set it active
+//     [...document.querySelectorAll('.operations__content')].forEach(elem =>
+//       elem.classList.remove('operations__content--active')
+//     );
+//     document
+//       .querySelector(`.operations__content--${e.target.dataset.tab}`)
+//       .classList.add('operations__content--active');
+//   }
+// });
+
+// second code
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsBtn = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabsBtn.forEach(btn => btn.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+  [...tabsContent]
+    .find(content =>
+      content.classList.contains(`operations__content--${clicked.dataset.tab}`)
+    )
+    ?.classList.add('operations__content--active');
+});
+
+//// Menu fading animation
+// first attempt

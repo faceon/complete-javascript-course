36b2f0fb379078d13b017e72e5908b2f2d605fc2
Seongwkim <faceon@gmail.com>
2023-01-26 18:08:52 +0900

    13 intersection observer

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -180,4 +180,85 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 //// Menu fading animation
+const nav = document.querySelector('.nav');
+
 // first attempt
+// const navLogo = document.querySelector('.nav__logo');
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// nav.addEventListener('mouseover', function (e) {
+//   const cursor = e.target;
+//   if (!cursor.classList.contains('nav__link')) return;
+//   navLinks.forEach(link => (link.style.opacity = 0.3));
+//   navLogo.style.opacity = 0.3;
+//   cursor.style.opacity = 1;
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   navLinks.forEach(link => (link.style.opacity = 1));
+//   navLogo.style.opacity = 1;
+// });
+
+// second attempt traversing
+// nav.addEventListener('mouseover', function (e) {
+//   const cursor = e.target;
+//   if (!cursor.classList.contains('nav__link')) return;
+//   cursor
+//     .closest('.nav')
+//     .querySelectorAll('.nav__link, .nav__logo')
+//     .forEach(elem => (elem.style.opacity = elem === cursor ? 1 : 0.3));
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   this.querySelectorAll('.nav__link, .nav__logo').forEach(
+//     elem => (elem.style.opacity = 1)
+//   );
+// });
+
+// third attempt using function
+const setOpacity = function (e) {
+  if (!e.target.classList.contains('nav__link')) return;
+  e.target
+    .closest('.nav')
+    .querySelectorAll('.nav__link, .nav__logo')
+    .forEach(elem => (elem.style.opacity = elem === e.target ? 1 : this));
+};
+nav.addEventListener('mouseover', setOpacity.bind(0.3));
+nav.addEventListener('mouseout', setOpacity.bind(1));
+
+//// Sticky nav
+const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+// low performance
+// window.addEventListener('scroll', function (e) {
+//   if (section1.getBoundingClientRect().top > 0) nav.classList.remove('sticky');
+//   else nav.classList.add('sticky');
+// });
+
+// high performance using intersection observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     // console.log(entry);
+//     if (!entry.isIntersecting) nav.classList.add('sticky');
+//     else nav.classList.remove('sticky');
+//   });
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0],
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(header);
+
+// Intersection observer API with a named callback
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) nav.classList.remove('sticky');
+  else nav.classList.add('sticky');
+};
+const observer = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+observer.observe(header);

799875d3b02293bebcd8d47fe588a1c630dcef6d
Seongwkim <faceon@gmail.com>
2023-02-02 17:33:18 +0900

    15 before architecturing

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,36 +11,83 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-const map = L.map('map');
+let map, mapEvent;
+map = L.map('map');
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+const coords = { lat: 37.5305, lng: 126.968 };
 
-const viewMap = function (lat, lng) {
-  map.setView([lat, lng], 13);
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+// navigator.geolocation?.getCurrentPosition(
+//   function (pos) {
+//     console.log(pos);
+//     const { latitude: lat, longitude: lng } = pos.coords;
+//     // console.log(`https://www.google.com/maps/@${lat},${lng}`);
+//     viewMap(lat, lng);
+//     markMap(lat, lng);
+//   },
+//   function (err) {
+//     console.log(err);
+//     return err;
+//   }
+// );
+
+const viewMap = function (coords) {
+  map.setView(coords, 20);
 };
 
-const markMap = function (lat, lng) {
-  L.marker([lat, lng]).addTo(map).bindPopup('Marked!').openPopup();
+const markMap = function (coords, markerContents) {
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        content: markerContents ?? '<div>TBD</div>',
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .openPopup();
 };
 
-navigator.geolocation?.getCurrentPosition(
-  function (pos) {
-    console.log(pos);
-    const { latitude: lat, longitude: lng } = pos.coords;
-    // console.log(`https://www.google.com/maps/@${lat},${lng}`);
-    viewMap(lat, lng);
-    markMap(lat, lng);
-  },
-  function (err) {
-    console.log(err);
-    return err;
-  }
-);
-
-map.on('click', mapEvent => {
+map.on('click', mapE => {
   // console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  markMap(lat, lng);
+  mapEvent = mapE;
+  // unhide form
+  form.classList.remove('hidden');
+  inputDistance.focus();
+  // console.log(inputDistance.value, inputDuration.value, inputCadence.value);
+
+  // markMap(coords);
+  // hide form
+  // if not, wait
 });
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  markMap(
+    mapEvent.latlng,
+    `<div>
+    ${inputType.value}, ${inputDistance.value}, 
+    ${inputDuration.value}, ${inputCadence.value}
+    </div>`
+  );
+  // Does the submitted form has all contents needed?
+  // if (
+  //   [inputDistance, inputDuration, inputCadence].some(
+  //     input => !Number(input.value) || Number(input.value) <= 0
+  //   )
+  // ) {
+  //   console.log('Input positive numbers');
+  //   return;
+  // }
+});
+
+inputType.addEventListener('change', function (e) {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+viewMap(coords);
+markMap(coords, '<h4>Hello world!<br /> This is a nice popup.</h4>');

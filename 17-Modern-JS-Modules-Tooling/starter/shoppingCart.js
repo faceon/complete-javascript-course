12fedcd7011c7826fd4ab75891e55a7b635a9c24
Seongwkim <faceon@gmail.com>
2023-02-12 13:06:39 +0900

    17 packaging, babel, polyfilling

--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,5 +1,5 @@
 // Exporting module
-console.log('Exporting module');
+// console.log('Exporting module');
 
 const shoppingCost = 10;
 const cart = [];
@@ -11,5 +11,21 @@ export const addToCart = function (product, quantity) {
 
 const totalPrice = 120;
 const totalQuantity = 15;
+
+export const wait = function (seconds) {
+  return new Promise(function (resolve, reject) {
+    // visualizing time passing
+    let dots = '';
+    const intervalId = setInterval(() => {
+      dots += '.';
+      console.log(dots);
+    }, 1000);
+    // resolve when time passed
+    setTimeout(() => {
+      clearInterval(intervalId);
+      resolve(`${seconds} seconds have passed`);
+    }, seconds * 1e3);
+  });
+};
 
 export { totalPrice as price, totalQuantity as tq };

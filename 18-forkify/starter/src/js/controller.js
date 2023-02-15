e031b99e6118f5cb9507e9568874d93ec61f9c5f
Seongwkim <faceon@gmail.com>
2023-02-15 18:13:59 +0900

    18 recipeView into an Object

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime';
 import * as model from './model';
 import recipeView from './views/recipeView';
 import errorView from './views/errorView';
-import icons from 'url:../img/icons.svg'; // Parcel 2
+// import icons from 'url:../img/icons.svg'; // Parcel 2
 
 const recipeContainer = document.querySelector('.recipe');
 const timeout = function (s) {
@@ -17,27 +17,16 @@ const timeout = function (s) {
 ////////////////////////////////////////////////////////
 // helper functions
 // spinner graphic to show while fetching
-const renderSpinner = function (parentEl) {
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML(
-    'afterbegin',
-    `<div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>`
-  );
-};
 
 ///////////////////////////////////////
 const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
-    renderSpinner(recipeContainer);
 
+    recipeView.renderSpinner();
     const recipe = await model.loadRecipe(id);
-    recipeView(recipe, recipeContainer);
+    recipeView.render(recipe);
   } catch (err) {
     errorView(err, recipeContainer);
   }
--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,100 +1,131 @@
 import icons from 'url:../../img/icons.svg'; // Parcel 2
+import { Fraction } from 'fractional';
 
-export default recipeView = function (data, targetEl) {
-  // recipe details
-  const recipeDiv = `
-  <figure class="recipe__fig">
-    <img src="${data.imgSrc}" alt="${data.title}" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${data.title}</span>
-    </h1>
-  </figure>
+class RecipeView {
+  #parentElement = document.querySelector('.recipe');
+  constructor() {}
 
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-clock"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${
-        data.cookingTime
-      }</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${data.icons}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        data.servings
-      }</span>
-      <span class="recipe__info-text">servings</span>
+  render = function (data) {
+    const markup = this.#getMarkup(data);
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('beforeend', markup);
+  };
 
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}#icon-plus-circle"></use>
-          </svg>
-        </button>
+  renderSpinner = function () {
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>`
+    );
+  };
+
+  #getMarkup = function (data) {
+    return `
+    <figure class="recipe__fig">
+      <img src="${data.imgSrc}" alt="${data.title}" class="recipe__img" />
+      <h1 class="recipe__title">
+        <span>${data.title}</span>
+      </h1>
+    </figure>
+  
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-clock"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${
+          data.cookingTime
+        }</span>
+        <span class="recipe__info-text">minutes</span>
       </div>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${data.icons}#icon-users"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--people">${
+          data.servings
+        }</span>
+        <span class="recipe__info-text">servings</span>
+  
+        <div class="recipe__info-buttons">
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}#icon-minus-circle"></use>
+            </svg>
+          </button>
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}#icon-plus-circle"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+  
+      <div class="recipe__user-generated">
+        <svg>
+          <use href="${icons}#icon-user"></use>
+        </svg>
+      </div>
+      <button class="btn--round">
+        <svg class="">
+          <use href="${icons}#icon-bookmark-fill"></use>
+        </svg>
+      </button>
     </div>
-
-    <div class="recipe__user-generated">
-      <svg>
-        <use href="${icons}#icon-user"></use>
-      </svg>
+  
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
+        ${data.ingredients
+          .map(
+            entry =>
+              `<li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                  <use href="${icons}#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${
+                  entry.quantity ? new Fraction(entry.quantity).toString() : ''
+                }</div>
+                <div class="recipe__description">
+                  <span class="recipe__unit">${entry.unit ?? ''}</span>
+                  ${entry.description}
+                </div>
+              </li>`
+          )
+          .join('')}
+      </ul>
     </div>
-    <button class="btn--round">
-      <svg class="">
-        <use href="${icons}#icon-bookmark-fill"></use>
-      </svg>
-    </button>
-  </div>
+  
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__publisher">${
+          data.publisher
+        }</span>. Please check out
+        directions at their website.
+      </p>
+      <a
+        class="btn--small recipe__btn"
+        href="${data.sourceUrl}"
+        target="_blank"
+      >
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>
+  `;
+  };
 
-  <div class="recipe__ingredients">
-    <h2 class="heading--2">Recipe ingredients</h2>
-    <ul class="recipe__ingredient-list">
-      ${data.ingredients
-        .map(
-          entry =>
-            `<li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${entry.quantity ?? ''}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${entry.unit ?? ''}</span>
-                ${entry.description}
-              </div>
-            </li>`
-        )
-        .join('')}
-    </ul>
-  </div>
+  #clear = function () {
+    this.#parentElement.innerHTML = '';
+  };
+}
 
-  <div class="recipe__directions">
-    <h2 class="heading--2">How to cook it</h2>
-    <p class="recipe__directions-text">
-      This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${data.publisher}</span>. Please check out
-      directions at their website.
-    </p>
-    <a
-      class="btn--small recipe__btn"
-      href="${data.sourceUrl}"
-      target="_blank"
-    >
-      <span>Directions</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </a>
-  </div>
-`;
-  targetEl.innerHTML = '';
-  targetEl.insertAdjacentHTML('beforeend', recipeDiv);
-};
+export default new RecipeView();
